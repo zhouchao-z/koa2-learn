88f8220bba7876c64e0040cd2058ebca0515267b
zhouchao-z <1051386008@qq.com>
2021-04-10 23:31:23 +0800

    feat:ejs

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
-    title: 'Hello Koa 2!'
+    title: 'Hello Koa 2!',
+    isMe: true
   })
 })
 
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
+router.get('/profile/:username', async (ctx, next) => {
+  const { username } = ctx.params
+  ctx.body = `<h2>hello world<h2>`
 })
 
 router.get('/json', async (ctx, next) => {
--- src/routes/users.js
+++ src/routes/users.js
@@ -10,4 +10,12 @@ router.get('/bar', function (ctx, next) {
   ctx.body = 'this is a users/bar response'
 })
 
+router.post('/person', async (ctx, next) => {
+  const { username, age } = ctx.request.body;
+  ctx.body = {
+    username,
+    age
+  }
+})
+
 module.exports = router
